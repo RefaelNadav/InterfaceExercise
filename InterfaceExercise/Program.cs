13278fd0a4011f7e6b0b84615ddc569537153c76
RefaelNadav <rn3857604@gmail.com>
2024-08-06 13:45:44 +0300

    exercise 3

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,9 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using InterfaceExercise;
 
-Console.WriteLine("Hello, World!");
-
 Student s1 = new Student("Moshe", "cohen", DateTime.Now);
+Teacher t1 = new Teacher("Yarden", "Shua", DateTime.Now.AddYears(-25));
 Console.WriteLine(s1.GetFullName());
 Console.WriteLine(s1.GetAge());
 Console.WriteLine(s1.GetRole());
+
+Console.WriteLine(t1.GetFullName());
+Console.WriteLine(t1.GetAge());
+Console.WriteLine(t1.GetRole());
+
+List<IPerson> persons = new List<IPerson>();
+persons.Add(s1);
+persons.Add(t1);
+
+foreach (IPerson item in persons)
+{
+    if(item is Teacher)
+    {
+        Console.WriteLine(item.GetFullName());
+    }
+    else
+    {
+        Console.WriteLine(item.GetAge());
+    }
+}
